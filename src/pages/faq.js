3277b143f6e534ce03f63dafd43ea107a3e6b4d4
BThvedt <bthvedt@gmail.com>
2023-12-31 18:40:56 -0600

    first commit

---
+++ src/pages/faq.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+// Step 2: Define your component
+const FAQ = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>FAQ</h1>
+        <p>Here are some FAQ's</p>
+      </main>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default FAQ
